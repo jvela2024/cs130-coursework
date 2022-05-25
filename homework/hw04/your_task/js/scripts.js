9a06565b8e9ede2fdbfe61935c9b845d484acc4b
jvela2024 <jessevela2024@u.northwestern.edu>
2022-05-25 18:51:30 -0500

    commit

--- homework/hw04/your_task/js/scripts.js
+++ homework/hw04/your_task/js/scripts.js
@@ -1,7 +1,7 @@
 const baseURL = 'https://www.apitutor.org/spotify/simple/v1/search';
 
 // Note: AudioPlayer is defined in audio-player.js
-const audioFile = 'https://p.scdn.co/mp3-preview/bfead324ff26bdd67bb793114f7ad3a7b328a48e?cid=9697a3a271d24deea38f8b7fbfa0e13c';
+const audioFile = 'https://p.scdn.co/mp3-preview/879c7106422b0b53852209da6a63210be7e09b01?cid=9697a3a271d24deea38f8b7fbfa0e13c';
 const audioPlayer = AudioPlayer('.player', audioFile);
 
 const search = (ev) => {
@@ -17,10 +17,36 @@ const search = (ev) => {
 }
 
 const getTracks = (term) => {
+    document.querySelector('#tracks').innerHTML = "";
     console.log(`
         get tracks from spotify based on the search term
         "${term}" and load them into the #tracks section 
         of the DOM...`);
+    console.log('about to fetch!');
+    fetch('https://www.apitutor.org/spotify/simple/v1/search?type=track&limit=5q=' + term)
+        .then(response => response.json())
+        .then(tracks => {
+            console.log(tracks);
+            if (tracks.length === 0) {
+                document.querySelector('#tracks').innerHTML +=`
+                    <p>No tracks found for "${track.name}"</p>
+                `;
+            }
+            for (const track of tracks) {
+                document.querySelector('#tracks').innerHTML +=`
+                <button class="track-item preview" data-preview-track="${track.preview_url}">
+                <img src="${track.artist.image_url}">
+                <i class="fas fa-play play-track" aria-hidden="true"></i>
+                <div class="label">
+                    <h2>${track.name}</h2>
+                    <p>
+                        ${track.artist.name}
+                    </p>
+                </div>
+            </button>
+                `;
+            }
+        });
 };
 
 const getAlbums = (term) => {
@@ -35,13 +61,36 @@ const getArtist = (term) => {
         get artists from spotify based on the search term
         "${term}" and load the first artist into the #artist section 
         of the DOM...`);
+    const elem = document.querySelector('#artist');
+    elem.innerHTML = ""; 
+    fetch(baseURL + "?type=artist&q=" + term)
+        .then(response => response.json())
+        .then(data => {
+            if (data.length > 0) {
+            const firstArtist = data[0];
+            elem.innerHTML += getArtistHTML(firstArtist);
+            }
+        })
 };
 
-const handleTrackClick = (ev) => {
-    const previewUrl = ev.currentTarget.getAttribute('data-preview-track');
-    console.log(previewUrl);
-}
+const getArtistHTML = (data) => {
+    // if (!data.image_url){
 
+    // }
+    return `
+    <section class="artist-card" id="${data.id}">
+    <div>
+        <img src="${data.image_url}">
+        <h2>${data.name}</h2>
+        <div class="footer">
+            <a href="${data.spotify_url}" target="_blank">
+                view on spotify
+            </a>
+        </div>
+    </div>
+</section>
+    `
+}
 document.querySelector('#search').onkeyup = (ev) => {
     // Number 13 is the "Enter" key on the keyboard
     console.log(ev.keyCode);
